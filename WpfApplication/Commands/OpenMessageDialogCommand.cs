519e2cd1c4d7d0fc2c2860ad4763c209c3689e47
akinari <akinari>
2014-09-22 20:35:59 +0900

    add open message dialog command

---
+++ WpfApplication/Commands/OpenMessageDialogCommand.cs
@@ -0,0 +1,25 @@
+﻿namespace WpfApplication.Commands
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Input;
+
+    public class OpenMessageDialogCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            var name = parameter as string;
+
+            var message = "こんにちは、" + name + "さん";
+
+            MessageBox.Show(message);
+        }
+    }
+}
---
+++ WpfApplication/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace WpfApplication.ViewModels
+{
+    using WpfApplication.Commands;
+    using WpfApplication.Models;
+
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            this.OpenMessageDialogCommand = new OpenMessageDialogCommand();
+            this.UserModel = new UserModel();
+        }
+
+        public OpenMessageDialogCommand OpenMessageDialogCommand { get; set; }
+
+        public UserModel UserModel { get; set; }
+    }
+}
